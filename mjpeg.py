6c9fe50aa77b0f3dcd3bf3e0a1a48b1618bd2bcc
lebarsfa <fabrice.le_bars@ensta-bretagne.fr>
2017-01-19 18:52:26 +0100

    Added Python sample client mjpeg.py and updated ReadMe.txt

---
+++ mjpeg.py
@@ -0,0 +1,10 @@
+﻿import cv2
+
+capture = cv2.VideoCapture("http://127.0.0.1:4014/?action=stream?dummy=param.mjpg")
+try:
+	while True:
+		ret, frame = capture.read()
+		cv2.imshow("Python MJPEG Client", frame)
+		cv2.waitKey(10)
+except KeyboardInterrupt:
+	print("Interrupted\n")
